b59073a38fa5897c7669f9c828682e3334c25729
gunnerDgd <ifstudiomail@naver.com>
2024-08-06 08:04:19 +0900

    [Fix] Forward Declaration (auto&&) missing - Invalid Copy Occurs

--- include/ap/trans/pack.hpp
+++ include/ap/trans/pack.hpp
@@ -20,31 +20,31 @@ namespace ap::trans                        {
         using trait = typename Trait;
     public:
         auto  push(str_t);
-        auto  pop (auto);
+        auto  pop (auto&);
 
         template <typename N, typename... T>
         auto  operator()(ap::pack<N, T...>&);
         auto  operator()(ap::meta::pack&);
 
-        template <is::pack_t V> void operator()(auto, ap::var<V>);
-        template <is::num_t  V> void operator()(auto, ap::var<V>);
-        void operator()(auto, ap::meta::var);
-        void var(auto, str_t, str_t);
+        template <is::pack_t V> void operator()(auto&&, ap::var<V>);
+        template <is::num_t  V> void operator()(auto&&, ap::var<V>);
+        void operator()(auto&&, ap::meta::var);
+        void var(auto&&, str_t, str_t);
 
-        void f64(auto, str_t);
-        void f32(auto, str_t);
+        void f64(auto&&, str_t);
+        void f32(auto&&, str_t);
 
-        void u64(auto, str_t);
-        void i64(auto, str_t);
+        void u64(auto&&, str_t);
+        void i64(auto&&, str_t);
 
-        void u32(auto, str_t);
-        void i32(auto, str_t);
+        void u32(auto&&, str_t);
+        void i32(auto&&, str_t);
 
-        void u16(auto, str_t);
-        void i16(auto, str_t);
+        void u16(auto&&, str_t);
+        void i16(auto&&, str_t);
 
-        void u8(auto, str_t);
-        void i8(auto, str_t);
+        void u8 (auto&&, str_t);
+        void i8 (auto&&, str_t);
     };
 }
 
@@ -56,7 +56,7 @@ namespace ap::trans                                                             
     template <is::pack_t V>
     void
         pack<T>::operator()
-            (auto self, ap::var<V> var)               {
+            (auto&& self, ap::var<V> var)               {
                 auto type = ap::type (var);
                 auto name = ap::name (var);
 
@@ -67,7 +67,7 @@ namespace ap::trans                                                             
     template <is::num_t V>
     void
         pack<T>::operator()
-            (auto self, ap::var<V> var)                                         {
+            (auto&& self, ap::var<V> var)                                         {
                 if constexpr (std::same_as<V, f64_t>) f64 (self, ap::name (var));
                 if constexpr (std::same_as<V, f32_t>) f32 (self, ap::name (var));
 
@@ -87,7 +87,7 @@ namespace ap::trans                                                             
     template <typename T>
     void
         pack<T>::operator()
-            (auto self, ap::meta::var var)      {
+            (auto&& self, ap::meta::var var)      {
                 auto type = ap::meta::type (var);
                 auto name = ap::meta::name (var);
 
@@ -118,25 +118,25 @@ namespace ap::trans                                                             
 }
 
 namespace ap::trans                                                                                            {
-    template <typename T> void pack<T>::var(auto self, str_t type, str_t name) { trait::var(self, type, name); }
+    template <typename T> void pack<T>::var(auto&& self, str_t type, str_t name) { trait::var(self, type, name); }
 
-    template <typename T> void pack<T>::f64(auto self, str_t name)  { trait::f64(self, name); }
-    template <typename T> void pack<T>::f32(auto self, str_t name)  { trait::f32(self, name); }
+    template <typename T> void pack<T>::f64(auto&& self, str_t name)  { trait::f64(self, name); }
+    template <typename T> void pack<T>::f32(auto&& self, str_t name)  { trait::f32(self, name); }
 
-    template <typename T> void pack<T>::u64(auto self, str_t name)  { trait::u64(self, name); }
-    template <typename T> void pack<T>::i64(auto self, str_t name)  { trait::i64(self, name); }
+    template <typename T> void pack<T>::u64(auto&& self, str_t name)  { trait::u64(self, name); }
+    template <typename T> void pack<T>::i64(auto&& self, str_t name)  { trait::i64(self, name); }
 
-    template <typename T> void pack<T>::u32(auto self, str_t name)  { trait::u32(self, name); }
-    template <typename T> void pack<T>::i32(auto self, str_t name)  { trait::i32(self, name); }
+    template <typename T> void pack<T>::u32(auto&& self, str_t name)  { trait::u32(self, name); }
+    template <typename T> void pack<T>::i32(auto&& self, str_t name)  { trait::i32(self, name); }
 
-    template <typename T> void pack<T>::u16(auto self, str_t name)  { trait::u16(self, name); }
-    template <typename T> void pack<T>::i16(auto self, str_t name)  { trait::i16(self, name); }
+    template <typename T> void pack<T>::u16(auto&& self, str_t name)  { trait::u16(self, name); }
+    template <typename T> void pack<T>::i16(auto&& self, str_t name)  { trait::i16(self, name); }
 
-    template <typename T> void pack<T>::u8 (auto self, str_t name)  { trait::u8 (self, name); }
-    template <typename T> void pack<T>::i8 (auto self, str_t name)  { trait::i8 (self, name); }
+    template <typename T> void pack<T>::u8 (auto&& self, str_t name)  { trait::u8 (self, name); }
+    template <typename T> void pack<T>::i8 (auto&& self, str_t name)  { trait::i8 (self, name); }
 
     template <typename T> auto pack<T>::push(str_t name) { return trait::push (name); }
-    template <typename T> auto pack<T>::pop (auto  self) { return trait::pop  (self); }
+    template <typename T> auto pack<T>::pop (auto& self) { return trait::pop  (self); }
 }
 
 namespace ap::trans                                                          {
