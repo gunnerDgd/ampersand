deb59788d09bacb7cec25cecf2819ab3fb39bce2
gunnerDgd <ifstudiomail@naver.com>
2024-08-06 19:07:52 +0900

    [Feature] Function can be added at ap::meta::src

--- include/ap/meta/src.hpp
+++ include/ap/meta/src.hpp
@@ -13,6 +13,7 @@ namespace ap::meta                                    {
     class src                                         {
         template <typename... T> friend class ap::pack;
         template <typename... T> friend class ap::var;
+        template <typename... T> friend class ap::fn;
 
         template <typename T>    friend class ari_eq;
         template <typename T>    friend class bit_eq;
@@ -20,20 +21,23 @@ namespace ap::meta                                    {
         using str_t = std::string_view;
 
         static void push(ap::meta::pack);
-        static void push(ap::meta::src);
         static void push(ap::meta::var);
         static void push(ap::meta::ope);
         static void push(ap::meta::op);
-        static auto pop ();
+        static void push(ap::meta::fn);
+        static void push();
 
-        static auto capture();
+        static std::optional<src> capture();
+        static std::optional<src> pop    ();
 
         src();
 
         using pack_t = std::unordered_map<str_t, ap::meta::pack>;
         using var_t  = std::unordered_map<str_t, ap::meta::var>;
+        using fun_t  = std::unordered_map<str_t, ap::meta::fn>;
         using ops_t  = std::list<ap::meta::op>;
         pack_t pack;
+        fun_t  fun;
         var_t  var;
         ops_t  ops;
     };
