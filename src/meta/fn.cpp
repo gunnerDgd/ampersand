c85e7bcfc02affc02674f4a09a01ef1b89a7f5b2
gunnerDgd <ifstudiomail@naver.com>
2024-08-06 19:11:42 +0900

    [Feature] ap::meta::fn added for function transpilation

---
+++ src/meta/fn.cpp
@@ -0,0 +1,17 @@
+#include <ap/core/fn.hpp>
+#include <ap/meta/fn.hpp>
+
+namespace ap::meta              {
+    fn::fn(ap::fn<void(void)> fn)
+        : name (ap::name (fn)),
+          src  (fn.src)
+    {}
+          
+}
+
+namespace ap::meta         {
+    std::string_view
+        name(meta::fn& fn) {
+            return fn.name;
+    }
+}
