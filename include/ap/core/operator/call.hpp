a79686bb4816f21abae9ae184b8fe2fd99f95d6e
gunnerDgd <ifstudiomail@naver.com>
2024-08-06 19:05:05 +0900

    [Feature] Call Operator Impelmented

--- include/ap/core/operator/call.hpp
+++ include/ap/core/operator/call.hpp
@@ -1,8 +1,39 @@
-//
-// Created by EnBay on 8/3/2024.
-//
+#ifndef AP_OPERATOR_CALL_HPP
+#define AP_OPERATOR_CALL_HPP
 
-#ifndef AMPERSAND_CALL_HPP
-#define AMPERSAND_CALL_HPP
+#include <ap/trait.hpp>
+#include <ap/meta/meta.hpp>
+#include <ap/meta/src.hpp>
 
-#endif //AMPERSAND_CALL_HPP
+namespace ap                                 {
+    template <opc C, typename... T> struct op;
+}
+
+namespace ap                                              {
+    template <typename T>
+    class call          {
+        using sub_t = T&;
+        sub_t sub;
+    public:
+        call(sub_t self) : sub (self) {}
+    public:
+        template <typename... Arg> auto operator()(Arg...);
+                                   auto operator()();
+    };
+
+    template <typename T>
+    template <typename... Arg> 
+    auto 
+        call<T>::operator()
+            (Arg... arg)                                    {
+                return op<opc::call, T, Arg...>(sub, arg...); 
+    }
+
+    template <typename T>
+    auto 
+        call<T>::operator()()           {
+            return op<opc::call, T>(sub); 
+    }
+}
+
+#endif
