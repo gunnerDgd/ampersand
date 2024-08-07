2e12961be111dd1de8e443e3396cc143c871f4ad
gunnerDgd <ifstudiomail@naver.com>
2024-08-07 19:47:46 +0900

    [Fix] ap::trans::mem : Wrong Trait Function called

--- include/ap/trans/ops/mem.hpp
+++ include/ap/trans/ops/mem.hpp
@@ -2,17 +2,20 @@
 #define AP_TRANS_MOVE_HPP
 
 #include <ap/trans/trans.hpp>
+#include <ap/meta/meta.hpp>
 #include <string_view>
 
 namespace ap::trans {
     template <typename Trait> 
     class mem <Trait>                           {
     public:
+        using str_t = std::string_view;
         using trait = Trait;
     public:
+        void push(auto ops, meta::type_id type) { trait::push(ops, type); }
+        void push(auto ops, str_t         type) { trait::push(ops, type); }
         void move(auto ops) { trait::move (ops); }
-        void call(auto ops) { trait::call (ops); }
-        void ret (auto ops) { trait::ret  (ops); }
+        void pop (auto ops) { trait::pop  (ops); }
     };
 }
 
