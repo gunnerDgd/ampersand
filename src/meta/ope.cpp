f0174b6116412329b45493ef13658285bcbd9729
gunnerDgd <ifstudiomail@naver.com>
2024-08-07 18:45:36 +0900

    Change] ap::meta::ope : Now can have ap::meta::func for Fuction Operand

--- src/meta/ope.cpp
+++ src/meta/ope.cpp
@@ -1,9 +1,10 @@
 #include <ap/meta/ope.hpp>
 
-namespace ap::meta                  {
-    ope::ope(var& arg) : sub (arg) {}
-    ope::ope(num& arg) : sub (arg) {}
-    ope::ope(op & arg) : sub (arg) {}
+namespace ap::meta                   {
+    ope::ope(func& arg) : sub (arg) {}
+    ope::ope(var&  arg) : sub (arg) {}
+    ope::ope(num&  arg) : sub (arg) {}
+    ope::ope(op &  arg) : sub (arg) {}
 
     std::optional<var>
         as_var(ope& self)       {
@@ -27,5 +28,24 @@ namespace ap::meta                  {
 
             if (sub.index() != 2) return std::nullopt;
             return std::get<2>(sub);
+    }
+
+    std::optional<func>
+        as_func(ope& self)      {
+            auto& sub = self.sub;
+
+            if (sub.index() != 3) return std::nullopt;
+            return std::get<3>(sub);
+    }
+}
+
+namespace ap::meta                                         {
+    ope::type
+        type(ope& ope)                                     {
+            if (ope.sub.index() == 0) return ope::type::var;
+            if (ope.sub.index() == 1) return ope::type::num;
+            if (ope.sub.index() == 2) return ope::type::op;
+            if (ope.sub.index() == 3) return ope::type::func;
+            return ope::type::none;
     }
 }
