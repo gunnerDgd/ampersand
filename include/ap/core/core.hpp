da6545d8c0a955c3d62301160d2f23cc8afa9d4a
gunnerDgd <ifstudiomail@naver.com>
2024-08-12 01:12:39 +0900

    [Feature] Array Feature Added (ap::var<T[]>)

--- include/ap/core/core.hpp
+++ include/ap/core/core.hpp
@@ -46,9 +46,37 @@ namespace ap                                            {
     template <std::size_t I, typename... T> auto arg (const ap::fn  <T...>&);
 }
 
+namespace ap                                                                                          {
+   // For var<is::pack_T T>
+    template <typename... P, is::pack_t T>                auto type(const ap::var<var<P...>, var<T>>&);
+    template <typename... P, is::pack_t T>                auto name(const ap::var<var<P...>, var<T>>&);
+    template <std::size_t I, typename... P, is::pack_t T> auto sub (const ap::var<var<P...>, var<T>>&);
+
+    // For var<is::var_t T, is::var_t U>
+    template <typename... P, typename T> auto self(const ap::var<ap::var<P...>, ap::var<T>>&);
+
+    // For var<is::bool_t T>, var <is::int_t T> and var <is::float_t T>
+
+    template <typename... P, is::bool_t T> auto name(const ap::var<var<P...>, var<T>>&);
+    template <typename... P, is::num_t T>  auto name(const ap::var<var<P...>, var<T>>&);
+    template <typename... P, is::bool_t T> auto type(const ap::var<var<P...>, var<T>>&);
+    template <typename... P, is::num_t T>  auto type(const ap::var<var<P...>, var<T>>&);
+}
+
+namespace ap                                                {
+    template <is::pack_t T1> auto type(const ap::var<T1[]>&);
+    template <is::pack_t T1> auto name(const ap::var<T1[]>&);
+    template <is::pack_t T1> auto len (const ap::var<T1[]>&);
+
+    template <is::num_t T1> auto type(const ap::var<T1[]>&);
+    template <is::num_t T1> auto name(const ap::var<T1[]>&);
+    template <is::num_t T1> auto len (const ap::var<T1[]>&);
+}
+
 namespace ap                           {
     template <typename T> class boolean;
     template <typename T> class call;
+    template <typename T> class move;
 
     template <typename T> class ari_eq;
     template <typename T> class bit_eq;
