0421df721a4d3028cbe737e71f06688a44942381
gunnerDgd <ifstudiomail@naver.com>
2024-08-12 01:17:04 +0900

    [Feature] Grammar Support (If / IfElse / Else / While / For) Added

---
+++ include/ap/grammar.hpp
@@ -0,0 +1,10 @@
+#ifndef AP_GRAMMAR_HPP
+#define AP_GRAMMAR_HPP
+
+#include "grammar/fn.hpp"
+#include "grammar/for.hpp"
+#include "grammar/if.hpp"
+#include "grammar/while.hpp"
+#include "grammar/let.hpp"
+
+#endif
