76b96f25d223a9dd554736cb82ca847321b94e55
gunnerDgd <ifstudiomail@naver.com>
2024-08-12 01:17:33 +0900

    [Feature] Grammar Support (If / IfElse / Else / While / For) Added [Move] Grammar Feature Moved under /ap/include/grammar

--- include/ap/core.hpp
+++ include/ap/core.hpp
@@ -1,59 +1,29 @@
 #ifndef AP_CORE_HPP
 #define AP_CORE_HPP
 
-#include "core/operator/ari.hpp"
-#include "core/operator/bit.hpp"
-#include "core/operator/boolean.hpp"
-#include "core/operator/move.hpp"
-#include "core/operator/call.hpp"
-#include "core/operator/ord.hpp"
+#include "core/operator.hpp"
 
-#include "core/pack.hpp"
 #include "core/move.hpp"
+#include "core/idx.hpp"
+
+#include "core/loop.hpp"
+#include "core/cond.hpp"
+
 #include "core/call.hpp"
+
 #include "core/ari.hpp"
+#include "core/boolean.hpp"
 #include "core/bit.hpp"
+#include "core/ord.hpp"
+#include "core/cmp.hpp"
+
 #include "core/bool.hpp"
 #include "core/float.hpp"
 #include "core/int.hpp"
 #include "core/var.hpp"
+#include "core/pack.hpp"
 #include "core/fn.hpp"
 
-
-namespace ap                                 {
-    template <is::type_t T>
-    auto let(T& type)                        {
-        return [type](std::string_view name) {
-            return var<T> { type, name };
-        };
-    }
-}
-
-namespace ap {
-    template <is::type_t R>
-    auto func(R)                               {
-        return [=]<is::var_t... A>(A... arg)   {
-        return [=](const char* name, auto fun) {
-        return fn<R(A...)> {
-            name ,
-            fun  ,
-            arg...
-        };
-        };
-        };
-    }
-
-    auto func()                                {
-        return [=]<is::var_t... A>(A... arg)   {
-        return [=](const char* name, auto fun) {
-        return fn<void(A...)> {
-            name ,
-            fun  ,
-            arg...
-        };
-        };
-        };
-    }
-}
+#include "meta/meta.hpp"
 
 #endif
