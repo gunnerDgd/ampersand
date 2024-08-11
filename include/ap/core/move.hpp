09eddd56289807f585d103ebd2a17fc55566a731
gunnerDgd <ifstudiomail@naver.com>
2024-08-12 01:12:29 +0900

    [Feature] Array Feature Added (ap::var<T[]>)

--- include/ap/core/move.hpp
+++ include/ap/core/move.hpp
@@ -60,4 +60,62 @@ namespace ap                          {
     };
 }
 
+
+
+namespace ap                                                    {
+    template <is::int_t T, typename U, typename V>
+        requires can::move <ap::var<T>, U>
+    struct op <opc::move, op < opc::idx, ap::var<T[]>, V >, U>
+
+        : public ari_eq <op <opc::move, op<opc::idx, ap::var<T[]>, V>, U>>,
+          public bit_eq <op <opc::move, op<opc::idx, ap::var<T[]>, V>, U>>,
+
+          public ari <op <opc::move, op<opc::idx, ap::var<T[]>, V>, U>>,
+          public bit <op <opc::move, op<opc::idx, ap::var<T[]>, V>, U>> {
+            op<opc::idx, ap::var<T[]>, V> self;
+            U                             arg;
+
+            op(op<opc::idx, ap::var<T[]>, V> self, U arg)
+                : ari_eq<op <opc::move, op<opc::idx, ap::var<T[]>, V>, U>>(*this),
+                  bit_eq<op <opc::move, op<opc::idx, ap::var<T[]>, V>, U>>(*this),
+
+                  ari<op <opc::move, op<opc::idx, ap::var<T[]>, V>, U>>(*this),
+                  bit<op <opc::move, op<opc::idx, ap::var<T[]>, V>, U>>(*this),
+
+                  self(self),
+                  arg (arg)
+            {}
+    };
+
+    template <is::float_t T, typename U, typename V>
+        requires can::move <op < opc::idx, ap::var<T>>, U>
+    struct op <opc::move,   op < opc::idx, ap::var<T[]>, V >, U>
+        : public ari_eq <op <opc::move, op<opc::idx, ap::var<T[]>, V>, U>>,
+          public ari    <op <opc::move, op<opc::idx, ap::var<T[]>, V>, U>>
+                                                                     {
+            op<opc::idx, ap::var<T[]>, V> self;
+            ap::var<U>                    arg;
+
+            op(op<opc::idx, ap::var<T[]>, V> self, U arg)
+                : ari_eq<op <opc::move, op<opc::idx, ap::var<T[]>, V>, U>>(*this),
+                  ari   <op <opc::move, op<opc::idx, ap::var<T[]>, V>, U>>(*this),
+
+                  self(self),
+                  arg (arg)
+            {}
+    };
+
+    template <is::pack_t T, typename U>
+    struct op <opc::move, op<opc::idx, ap::var<T[]>, U>, ap::var<T>>
+                                          {
+        op<opc::idx, ap::var<T[]>, U> self;
+        ap::var<T>                    arg;
+
+        op (op<opc::idx, ap::var<T[]>, U> self, ap::var<T> arg)
+            : self (self),
+              arg  (arg)
+        {}
+    };
+}
+
 #endif
--- include/ap/core/var.hpp
+++ include/ap/core/var.hpp
@@ -1,14 +1,7 @@
 #ifndef AP_VAR_HPP
 #define AP_VAR_HPP
 
-#include <ap/core/operator/boolean.hpp>
-#include <ap/core/operator/ord.hpp>
-#include <ap/core/operator/cmp.hpp>
-
-#include <ap/core/operator/move.hpp>
-#include <ap/core/operator/ari.hpp>
-#include <ap/core/operator/bit.hpp>
-
+#include <ap/core/operator.hpp>
 #include <ap/core/float.hpp>
 #include <ap/core/int.hpp>
 #include <ap/core/pack.hpp>
@@ -25,7 +18,8 @@ namespace ap                          {
 
     template <is::bool_t T>
     class var<T> 
-        : public boolean <var<T>>     {
+        : public boolean <var<T>>,
+          public move    <var<T>>                                  {
         using str_t = std::string_view;
         
         template <is::bool_t V> friend auto type(const ap::var<V>&);
@@ -35,6 +29,7 @@ namespace ap                          {
     public:
         var(T, str_t name)
             : boolean <var<T>> (*this),
+              move    <var<T>> (*this),
               name (name)
         {
             meta::src::push (meta::push(*this));
@@ -55,14 +50,14 @@ namespace ap                          {
           public boolean<var<T>>,
           public cmp    <var<T>>,
           public ord    <var<T>>
-                                    {
-        using str = std::string_view;
+                                                                  {
+        using str_t = std::string_view;
 
         template <is::num_t V> friend auto type(const ap::var<V>&);
         template <is::num_t V> friend auto name(const ap::var<V>&);
-        str name;
+        str_t name;
     public:
-        var(T, str name)
+        var(T, str_t name)
             : ari_eq <var <T>> (*this),
               bit_eq <var <T>> (*this),
               
@@ -99,14 +94,14 @@ namespace ap                          {
           public cmp    <var<T>>,
           public ord    <var<T>>
 
-                                    {
-        using str = std::string_view;
-
+                                                                  {
         template <is::num_t V> friend auto type(const ap::var<V>&);
         template <is::num_t V> friend auto name(const ap::var<V>&);
-        str name;
+
+        using str_t = std::string_view;
+        str_t name;
     public:
-        var(T, str name)
+        var(T, str_t name)
             : ari_eq <var <T>> (*this),
               ari    <var <T>> (*this),
 
@@ -127,37 +122,19 @@ namespace ap                          {
                 auto&  self = static_cast<ap::move<var<T>>&>(*this);
                 return self = arg;
         }
-    };
-
-
-
-    template <is::pack_t P, typename T>
-    class var <var<P>, var<T>> : public var<T> {
-        friend class var<P>;
-
-        using  str = std::string_view;
-        var<P> self;
-
-        template <is::pack_t rP, typename rT> friend auto self(const ap::var<ap::var<rP>, ap::var<rT>>&);
-        var(var<P>, var<T>);
-    };
-
-    template <is::pack_t P, typename T>
-    ap::var<var<P>, var<T>>::var(var<P> sup, var<T> var)
-        : var<T>(var),
-          self  (sup)
-    {}
-          
+    };     
 
     template <is::pack_t T>
-    class var<T> : public move <var<T>> {
-        using str = std::string_view;
-
-        template <is::pack_t V, std::size_t I> friend auto sub (const ap::var<V>&);
+    class var<T> 
+        : public move <var<T>> 
+                                                                                  {    
+        template <std::size_t I, is::pack_t V> friend auto sub (const ap::var<V>&);
         template <is::pack_t V>                friend auto type(const ap::var<V>&);
         template <is::pack_t V>                friend auto name(const ap::var<V>&);
-        str name;
-        T   pack;
+
+        using str_t = std::string_view;
+        str_t name;
+        T     pack;
     public:
         auto 
             operator=(var<T>& arg)                                 {
@@ -167,7 +144,7 @@ namespace ap                          {
         }
 
     public:
-        var(T pack, str name)
+        var(T pack, str_t name)
             : move <var<T>> (*this),
               name (name),
               pack (pack)
@@ -182,6 +159,7 @@ namespace ap                          {
     template <is::pack_t T> var(T, std::string_view) -> var<T>;
     template <is::num_t  T> var(T, std::string_view) -> var<T>;
 }
+
 
 namespace ap                                                                    {
     // For var<is::pack_T T>
@@ -204,9 +182,268 @@ namespace ap                                                                    
             return var.self;
     }
 
-    // For var <is::int_t T> and var <is::float_t T>
-    template <is::num_t T> auto type(const ap::var<T>& var) { return var.type; }
-    template <is::num_t T> auto name(const ap::var<T>& var) { return var.name; }
+    // For var<is::bool_t T>, var <is::int_t T> and var <is::float_t T>
+    template <is::bool_t T> auto type(const ap::var<T>& var) { return T(); }
+    template <is::num_t T>  auto type(const ap::var<T>& var) { return T(); }
+
+    template <is::bool_t T> auto name(const ap::var<T>& var) { return var.name; }
+    template <is::num_t T>  auto name(const ap::var<T>& var) { return var.name; }
+}
+
+
+
+
+// For Pack Referenced Variables
+namespace ap                                   {
+    template <typename... P, is::bool_t T>
+    class var <var<P...>, var<T>> 
+        : public boolean <var<var<P...>, var<T>>>,
+          public move    <var<var<P...>, var<T>>>
+                                                                                                              {
+        template <typename... T1, typename   T2> friend auto self(const ap::var<ap::var<T1...>, ap::var<T2>>&);
+        template <typename... T1, is::bool_t T2> friend auto type(const ap::var<ap::var<T1...>, ap::var<T2>>&);
+        template <typename... T1, is::bool_t T2> friend auto name(const ap::var<ap::var<T1...>, ap::var<T2>>&);
+
+        friend class var<P>;
+        var<P...> self;
+        var<T>    sub;
+
+        var(var<P...> sup, var<T> sub)
+            : boolean <ap::var<ap::var<P...>, ap::var<T>>>(*this),
+              move    <ap::var<ap::var<P...>, ap::var<T>>>(*this),
+
+              self (sup),
+              sub  (sub)
+                {}
+    };
+
+    template <typename... P, is::int_t T>
+    class var<var<P...>, var<T>>
+        : public ari_eq <var<var<P...>, var<T>>>,
+          public ari    <var<var<P...>, var<T>>>,
+    
+          public bit_eq <var<var<P...>, var<T>>>,
+          public bit    <var<var<P...>, var<T>>>,
+    
+          public move   <var<var<P...>, var<T>>>,
+          
+          public boolean<var<var<P...>, var<T>>>,
+          public cmp    <var<var<P...>, var<T>>>,
+          public ord    <var<var<P...>, var<T>>>
+                                                                                                             {
+        template <typename... T1, typename  T2> friend auto self(const ap::var<ap::var<T1...>, ap::var<T2>>&);
+        template <typename... T1, is::num_t T2> friend auto type(const ap::var<ap::var<T1...>, ap::var<T2>>&);
+        template <typename... T1, is::num_t T2> friend auto name(const ap::var<ap::var<T1...>, ap::var<T2>>&);
+
+        friend class var<P...>;
+        var<P...> self;
+        var<T>    sub;
+        
+    public:
+        var(var<P...> sup, var<T> sub)
+            : ari_eq <ap::var <ap::var<P...>, ap::var<T>>> (*this),
+              bit_eq <ap::var <ap::var<P...>, ap::var<T>>> (*this),
+              
+              ari    <ap::var <ap::var<P...>, ap::var<T>>> (*this),
+              bit    <ap::var <ap::var<P...>, ap::var<T>>> (*this),
+
+              move   <ap::var <ap::var<P...>, ap::var<T>>> (*this),
+              
+              boolean<ap::var <ap::var<P...>, ap::var<T>>> (*this),
+              cmp    <ap::var <ap::var<P...>, ap::var<T>>> (*this),
+              ord    <ap::var <ap::var<P...>, ap::var<T>>> (*this),
+
+              self (sup),
+              sub  (sub)
+                {}
+
+        auto 
+            operator=(auto arg)                                                    {
+                auto&  self = static_cast<ap::move<var<var<P...>, var<T>>>&>(*this);
+                return self = arg;
+        }
+    };
+
+    template <typename... P, is::float_t T>
+    class var<var<P...>, var<T>>
+        : public ari_eq <var<var<P...>, var<T>>>,
+          public ari    <var<var<P...>, var<T>>>,
+                    
+          public move   <var<var<P...>, var<T>>>,
+
+          public boolean<var<var<P...>, var<T>>>,
+          public cmp    <var<var<P...>, var<T>>>,
+          public ord    <var<var<P...>, var<T>>>
+
+                                                                                                             {
+        template <typename... T1, typename  T2> friend auto self(const ap::var<ap::var<T1...>, ap::var<T2>>&);
+        template <typename... T1, is::num_t T2> friend auto type(const ap::var<ap::var<T1...>, ap::var<T2>>&);
+        template <typename... T1, is::num_t T2> friend auto name(const ap::var<ap::var<T1...>, ap::var<T2>>&);
+
+        friend class var<P...>;
+        var<P...> self;
+        var<T>    sub;
+
+    public:
+        var(var<P...> sup, var<T> sub)
+            : ari_eq <ap::var <ap::var<P...>, ap::var<T>>> (*this),
+              ari    <ap::var <ap::var<P...>, ap::var<T>>> (*this),
+
+              move   <ap::var <ap::var<P...>, ap::var<T>>> (*this),
+
+              boolean<ap::var <ap::var<P...>, ap::var<T>>> (*this),
+              cmp    <ap::var <ap::var<P...>, ap::var<T>>> (*this),
+              ord    <ap::var <ap::var<P...>, ap::var<T>>> (*this),
+
+              self (sup),
+              sub  (sub)
+                {}
+
+        auto 
+            operator=(auto arg)                                                                {
+                auto&  self = static_cast<ap::move<ap::var<ap::var<P...>, ap::var<T>>>&>(*this);
+                return self = arg;
+        }
+    };     
+
+    template <typename... P, is::pack_t T>
+    class var<var<P...>, var<T>>
+        : public move <var<var<P...>, var<T>>>                                        
+                                                                                                                             {
+        template <typename... T1, typename   T2>                friend auto self(const ap::var<ap::var<T1...>, ap::var<T2>>&);
+        template <typename... T1, is::pack_t T2>                friend auto type(const ap::var<ap::var<T1...>, ap::var<T2>>&);
+        template <typename... T1, is::pack_t T2>                friend auto name(const ap::var<ap::var<T1...>, ap::var<T2>>&);
+        template <typename... T1, is::pack_t T2, std::size_t I> friend auto sub (const ap::var<ap::var<T1...>, ap::var<T2>>&);
+        var<P...> self;
+        var<T>    sub;
+        
+    public:
+        auto 
+            operator=(var<var<P...>, var<T>>& arg)                                 {
+                auto&  self = static_cast<ap::move<var<var<P...>, var<T>>>&>(*this);
+                auto&  move = static_cast<ap::move<var<var<P...>, var<T>>>&>(arg);
+                return self = move;
+        }
+
+    public:
+        var(var<P...> sup, var<T> sub)
+            : move <ap::var<ap::var<P...>, ap::var<T>>> (*this),
+
+              self(sup),
+              sub (sub)
+                {}
+    };
+
+    template <typename... P, is::bool_t T>  var(ap::var<P...>, var<T>) -> var <var<P...>, var<T>>;
+    template <typename... P, is::int_t T>   var(ap::var<P...>, var<T>) -> var <var<P...>, var<T>>;
+    template <typename... P, is::float_t T> var(ap::var<P...>, var<T>) -> var <var<P...>, var<T>>;
+    template <typename... P, is::pack_t T>  var(ap::var<P...>, var<T>) -> var <var<P...>, var<T>>;
+}
+
+
+
+namespace ap                                                                                                                           {
+    // For var<is::pack_T T>
+    template <typename... P, is::pack_t T>                auto type(const ap::var<var<P...>, var<T>>& var) { return ap::type(var.sub); }
+    template <typename... P, is::pack_t T>                auto name(const ap::var<var<P...>, var<T>>& var) { return ap::name(var.sub); }
+    template <std::size_t I, typename... P, is::pack_t T> auto sub (const ap::var<var<P...>, var<T>>& var) { return ap::sub (var.sub); }
+
+    // For var<is::var_t T, is::var_t U>
+    template <typename... P, typename T> auto self(const ap::var<ap::var<P...>, ap::var<T>>& var) { return var.self; }
+
+    // For var<is::bool_t T>, var <is::int_t T> and var <is::float_t T>
+
+    template <typename... P, is::bool_t T> auto name(const ap::var<var<P...>, var<T>>& var) { return ap::name (var.sub); }
+    template <typename... P, is::num_t T>  auto name(const ap::var<var<P...>, var<T>>& var) { return ap::name (var.sub); }
+    template <typename... P, is::bool_t T> auto type(const ap::var<var<P...>, var<T>>& var) { return T(); }
+    template <typename... P, is::num_t T>  auto type(const ap::var<var<P...>, var<T>>& var) { return T(); }
+}
+
+
+// For Array Type Support
+namespace ap                                                          {
+    template <is::num_t T>
+    class var<T[]> 
+        : public move <var<T[]>>,
+          public idx  <var<T[]>>
+                                                                      {
+        using str_t = std::string_view;
+        using len_t = std::uint64_t;
+
+        template <is::num_t T1> friend auto type(const ap::var<T1[]>&);
+        template <is::num_t T1> friend auto name(const ap::var<T1[]>&);
+        template <is::num_t T1> friend auto len (const ap::var<T1[]>&);
+        str_t name;
+        len_t len;
+    public:
+        auto 
+            operator=(var<T[]>& arg)                                 {
+                auto&  self = static_cast<ap::move<var<T[]>>&>(*this);
+                auto&  move = static_cast<ap::move<var<T[]>>&>(arg);
+                return self = move;
+        }
+
+    public:
+        var(T, str_t name, len_t len)
+            : move <var<T[]>>(*this),
+              idx  <var<T[]>>(*this),
+
+              name (name),
+              len  (len)
+        {
+            meta::src::push(meta::push(*this));
+            meta::src::push(meta::var (*this));
+        }
+    };
+
+    template <is::pack_t T>
+    class var<T[]> 
+        : public move <var<T[]>>,
+          public idx  <var<T[]>>
+                                                                       {
+        using str_t = std::string_view;
+        using len_t = std::uint64_t;
+        T pack;
+
+        template <is::pack_t T1> friend auto type(const ap::var<T1[]>&);
+        template <is::pack_t T1> friend auto name(const ap::var<T1[]>&);
+        template <is::pack_t T1> friend auto len (const ap::var<T1[]>&);
+        str_t name;
+        len_t len;
+    public:
+        auto 
+            operator=(var<T[]>& arg)                                 {
+                auto&  self = static_cast<ap::move<var<T[]>>&>(*this);
+                auto&  move = static_cast<ap::move<var<T[]>>&>(arg);
+                return self = move;
+        }
+
+    public:
+        var(T pack, str_t name, len_t len)
+            : move <var<T[]>>(*this),
+              idx  <var<T[]>>(*this),
+                
+              pack (pack),
+              name (name),
+              len  (len)
+        {
+            meta::src::push(meta::push(*this));
+            meta::src::push(meta::var (*this));
+        }
+    };
+
+    template <is::pack_t T> var (T, std::string_view, std::uint64_t) -> var <T[]>;
+    template <is::num_t  T> var (T, std::string_view, std::uint64_t) -> var <T[]>;
+}
+
+namespace ap                                                                        {
+    template <is::pack_t T1> auto type(const ap::var<T1[]>& var) { return var.pack; }
+    template <is::pack_t T1> auto name(const ap::var<T1[]>& var) { return var.name; }
+    template <is::pack_t T1> auto len (const ap::var<T1[]>& var) { return var.len ; }
+
+    template <is::num_t T1> auto type(const ap::var<T1[]>& var) { return T1(); }
+    template <is::num_t T1> auto name(const ap::var<T1[]>& var) { return var.name; }
+    template <is::num_t T1> auto len (const ap::var<T1[]>& var) { return var.len ; }
 }
 
 #endif
