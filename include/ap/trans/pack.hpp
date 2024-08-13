2fce04624d941f64abab0697084e7704ae0607d1
gunnerDgd <ifstudiomail@naver.com>
2024-08-13 17:35:10 +0900

    [Change] Dows not requires pack::trait anymore

--- include/ap/trans/pack.hpp
+++ include/ap/trans/pack.hpp
@@ -11,17 +11,14 @@
 
 #include <string_view>
 
-namespace ap::trans                        {
+namespace ap::trans                     {
     
-    template <typename Trait>
-    class pack <Trait>                     {
+    template <typename T>
+    class pack <T>                      {
     public:
         using str_t = std::string_view;
-        using trait = typename Trait;
     public:
-        auto make(str_t);
-
-        auto operator()(ap::meta::pack&);
+        auto operator()(ap::meta::pack);
 
         void var(auto&&, ap::meta::var);
         void var(auto&&, str_t, str_t);
@@ -53,62 +50,61 @@ namespace ap::trans                                                             
             (auto&& self, ap::meta::var var)      {
                 auto type = ap::meta::type (var);
                 auto name = ap::meta::name (var);
+                if (!name.has_value()) return;
 
                 if (type.index() == 0) goto pack;
                 if (type.index() == 1) goto num;
 
-        pack:   trait::var(self, std::get<0>(type), name);
+        pack:   self.var(std::get<0>(type), name.value());
                 return;
         num:    auto idx  = std::get<1>(type);
 
-                switch (idx)                                                        {
-                    case meta::type_id::f64: f64 (self, ap::meta::name (var)); break;
-                    case meta::type_id::f32: f32 (self, ap::meta::name (var)); break;
+                switch (idx)                                               {
+                    case meta::type_id::f64: self.f64 (name.value()); break;
+                    case meta::type_id::f32: self.f32 (name.value()); break;
 
-                    case meta::type_id::u64: u64 (self, ap::meta::name (var)); break;
-                    case meta::type_id::i64: i64 (self, ap::meta::name (var)); break;
+                    case meta::type_id::u64: self.u64 (name.value()); break;
+                    case meta::type_id::i64: self.i64 (name.value()); break;
 
-                    case meta::type_id::u32: u32 (self, ap::meta::name (var)); break;
-                    case meta::type_id::i32: i32 (self, ap::meta::name (var)); break;
+                    case meta::type_id::u32: self.u32 (name.value()); break;
+                    case meta::type_id::i32: self.i32 (name.value()); break;
 
-                    case meta::type_id::u16: u16 (self, ap::meta::name (var)); break;
-                    case meta::type_id::i16: i16 (self, ap::meta::name (var)); break;
+                    case meta::type_id::u16: self.u16 (name.value()); break;
+                    case meta::type_id::i16: self.i16 (name.value()); break;
 
-                    case meta::type_id::u8 : u8  (self, ap::meta::name (var)); break;
-                    case meta::type_id::i8 : i8  (self, ap::meta::name (var)); break;
+                    case meta::type_id::u8 : self.u8  (name.value()); break;
+                    case meta::type_id::i8 : self.i8  (name.value()); break;
                 }
     }
 }
 
-namespace ap::trans                                                                                              {
-    template <typename T> void pack<T>::var(auto&& self, str_t type, str_t name) { trait::var(self, type, name); }
+namespace ap::trans                                                                                      {
+    template <typename T> void pack<T>::var(auto&& self, str_t type, str_t name) { self.var(type, name); }
 
-    template <typename T> void pack<T>::f64(auto&& self, str_t name)  { trait::f64(self, name); }
-    template <typename T> void pack<T>::f32(auto&& self, str_t name)  { trait::f32(self, name); }
+    template <typename T> void pack<T>::f64(auto&& self, str_t name)  { self.f64(name); }
+    template <typename T> void pack<T>::f32(auto&& self, str_t name)  { self.f32(name); }
 
-    template <typename T> void pack<T>::u64(auto&& self, str_t name)  { trait::u64(self, name); }
-    template <typename T> void pack<T>::i64(auto&& self, str_t name)  { trait::i64(self, name); }
+    template <typename T> void pack<T>::u64(auto&& self, str_t name)  { self.u64(name); }
+    template <typename T> void pack<T>::i64(auto&& self, str_t name)  { self.i64(name); }
 
-    template <typename T> void pack<T>::u32(auto&& self, str_t name)  { trait::u32(self, name); }
-    template <typename T> void pack<T>::i32(auto&& self, str_t name)  { trait::i32(self, name); }
+    template <typename T> void pack<T>::u32(auto&& self, str_t name)  { self.u32(name); }
+    template <typename T> void pack<T>::i32(auto&& self, str_t name)  { self.i32(name); }
 
-    template <typename T> void pack<T>::u16(auto&& self, str_t name)  { trait::u16(self, name); }
-    template <typename T> void pack<T>::i16(auto&& self, str_t name)  { trait::i16(self, name); }
+    template <typename T> void pack<T>::u16(auto&& self, str_t name)  { self.u16(name); }
+    template <typename T> void pack<T>::i16(auto&& self, str_t name)  { self.i16(name); }
 
-    template <typename T> void pack<T>::u8 (auto&& self, str_t name)  { trait::u8 (self, name); }
-    template <typename T> void pack<T>::i8 (auto&& self, str_t name)  { trait::i8 (self, name); }
-
-    template <typename T> auto pack<T>::make(str_t name) { return trait::make(name); }
+    template <typename T> void pack<T>::u8 (auto&& self, str_t name)  { self.u8 (name); }
+    template <typename T> void pack<T>::i8 (auto&& self, str_t name)  { self.i8 (name); }
 }
 
-namespace ap::trans                                   {
-    template <typename Trait>
+namespace ap::trans                                {
+    template <typename T>
     auto
-        pack<Trait>::operator()
-            (ap::meta::pack& arg)                     {
-                auto ret = make (ap::meta::name (arg));
-                for (auto&& pos : arg.sub)            {
-                    (*this)(ret, pos);
+        pack<T>::operator()
+            (ap::meta::pack arg)                   {
+                auto ret = T (ap::meta::name (arg));
+                for (auto&& pos : arg.sub)         {
+                    this->var(ret, pos);
                 }
 
                 return ret;
